bc065bd5c53714b6d34ef9613dcb0819afd80ef3
YoYoMastr <petertheyoyomaster@gmail.com>
2026-06-14 16:02:06 -0500

    Test

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,5 +9,5 @@ public static class ModHelperData
     public const string Description = "press F8 to change the round (Mod orginally made by Timmotheeee editted by me)";
 
     public const string RepoOwner = "YoYoMastr (YoYoMaster)"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "round_changer"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "round changer"; // TODO add your repo name here, also in the download url in README.md
 }
