7444852774f9a7846c3b4ae14f2d99993da0fd71
YoYoMastr <petertheyoyomaster@gmail.com>
2026-06-14 16:47:08 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace round_changer;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "55.0";
-    public const string Version = "1.0";
-    public const string Name = "round changer";
+    public const string Version = "1.1";
+    public const string Name = "round changer but better";
 
-    public const string Description = "press F8 to change the round (Mod orginally made by Timotheeee editted by me)";
+    public const string Description = "press F8 to change the round (Mod orginally made by Timotheeee edited by me)";
 
-    public const string RepoOwner = "YoYoMastr (YoYoMaster)"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoOwner = "YoYoMastr"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "round changer"; // TODO add your repo name here, also in the download url in README.md
 }
