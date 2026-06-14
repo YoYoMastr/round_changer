005265f25a9b0c7921a1aa823417f7951df8476d
YoYoMastr <petertheyoyomaster@gmail.com>
2026-06-14 17:05:02 -0500

    Modify description for round changer mod

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.1";
     public const string Name = "round changer but better";
 
-    public const string Description = "press F8 to change the round (Mod orginally made by Timotheeee edited by me)";
+    public const string Description = "press F8 to change the round now has no limit on what round you can go to (Mod orginally made by Timotheeee edited by me)";
 
     public const string RepoOwner = "YoYoMastr"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "round changer"; // TODO add your repo name here, also in the download url in README.md
